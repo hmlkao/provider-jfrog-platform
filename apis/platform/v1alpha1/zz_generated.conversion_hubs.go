a3f6d052743d9675bb1e50166e99a107cc1bb335
Ondrej Homolka <ondrej.homolka@merck.com>
2025-04-22 16:28:21 +0200

    Add files generated for ReverseProxy resource

--- apis/platform/v1alpha1/zz_generated.conversion_hubs.go
+++ apis/platform/v1alpha1/zz_generated.conversion_hubs.go
@@ -10,4 +10,7 @@ package v1alpha1
 func (tr *Group) Hub() {}
 
 // Hub marks this type as a conversion hub.
+func (tr *ReverseProxy) Hub() {}
+
+// Hub marks this type as a conversion hub.
 func (tr *SAMLSettings) Hub() {}
