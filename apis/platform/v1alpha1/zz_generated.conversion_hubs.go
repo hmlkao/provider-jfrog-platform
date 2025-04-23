a6e0c92d9b4da5e864d35ffbb7a41106e396864f
Ondrej Homolka <ondrej.homolka@merck.com>
2025-04-23 11:23:47 +0200

    Add files generated for HTTPSSOSettings resource

--- apis/platform/v1alpha1/zz_generated.conversion_hubs.go
+++ apis/platform/v1alpha1/zz_generated.conversion_hubs.go
@@ -10,6 +10,9 @@ package v1alpha1
 func (tr *Group) Hub() {}
 
 // Hub marks this type as a conversion hub.
+func (tr *HTTPSSOSettings) Hub() {}
+
+// Hub marks this type as a conversion hub.
 func (tr *OIDCConfiguration) Hub() {}
 
 // Hub marks this type as a conversion hub.
