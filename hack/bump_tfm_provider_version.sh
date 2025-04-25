099a61d3fd0caa8b77e11d00281b3ae2846e49fc
Ondrej Homolka <ondrej.homolka@merck.com>
2025-04-25 17:39:17 +0200

    Improve Bump version workflow

--- hack/bump_tfm_provider_version.sh
+++ hack/bump_tfm_provider_version.sh
@@ -1,14 +1,28 @@
 #!/usr/bin/env bash
 
+# Bump version of Terraform provider
+
 set -euo pipefail
 IFS=$'\n\t'
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-current_version=$(grep 'export TERRAFORM_PROVIDER_VERSION' < "$dir/../Makefile" | cut -d' ' -f4)
-new_version=$(curl -sSL https://api.github.com/repos/jfrog/terraform-provider-platform/releases/latest | jq -r '.tag_name' | tr -d 'v')
+current_version=${1}
+if [[ -z "$current_version" ]]; then
+  echo "::error Current version is empty"
+  exit 1
+fi
+new_version=${2}
+if [[ -z "$new_version" ]]; then
+  echo "::error New version is empty"
+  exit 1
+fi
 
-sed -i "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|" "$dir/../Makefile"
-sed -i "s|terraform-provider-platform_v${current_version}|terraform-provider-platform_v${new_version}|" "$dir/../Makefile"
-sed -i "s|jfrog/platform v${current_version}|jfrog/platform v${new_version}|g" "$dir/../README.md"
-sed -i "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "$dir/../README.md"
-sed -i "s|jfrog/platform/${current_version}|jfrog/platform/${new_version}|" "$dir/../README.md"
+main() {
+  sed -i "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|" "$dir/../Makefile"
+  sed -i "s|terraform-provider-platform_v${current_version}|terraform-provider-platform_v${new_version}|" "$dir/../Makefile"
+  sed -i "s|jfrog/platform v${current_version}|jfrog/platform v${new_version}|g" "$dir/../README.md"
+  sed -i "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "$dir/../README.md"
+  sed -i "s|jfrog/platform/${current_version}|jfrog/platform/${new_version}|" "$dir/../README.md"
+}
+
+main "$@"
