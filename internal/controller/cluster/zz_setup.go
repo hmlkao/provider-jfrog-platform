540f6366f18f968b5878e047db1d0e13d8c62f9a
Ondrej Homolka <ondrej.homolka@merck.com>
2025-09-04 10:54:33 +0200

    Add generated files for Crossplane v2 support

---
+++ internal/controller/cluster/zz_setup.go
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: 2024 The Crossplane Authors <https://crossplane.io>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package controller
+
+import (
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/crossplane/upjet/v2/pkg/controller"
+
+	awsiamrole "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/awsiamrole"
+	crowdsettings "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/crowdsettings"
+	globalrole "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/globalrole"
+	group "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/group"
+	httpssosettings "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/httpssosettings"
+	oidcconfiguration "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/oidcconfiguration"
+	reverseproxy "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/reverseproxy"
+	samlsettings "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/platform/samlsettings"
+	providerconfig "github.com/hmlkao/provider-jfrog-platform/internal/controller/cluster/providerconfig"
+)
+
+// Setup creates all controllers with the supplied logger and adds them to
+// the supplied manager.
+func Setup(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		awsiamrole.Setup,
+		crowdsettings.Setup,
+		globalrole.Setup,
+		group.Setup,
+		httpssosettings.Setup,
+		oidcconfiguration.Setup,
+		reverseproxy.Setup,
+		samlsettings.Setup,
+		providerconfig.Setup,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// SetupGated creates all controllers with the supplied logger and adds them to
+// the supplied manager gated.
+func SetupGated(mgr ctrl.Manager, o controller.Options) error {
+	for _, setup := range []func(ctrl.Manager, controller.Options) error{
+		awsiamrole.SetupGated,
+		crowdsettings.SetupGated,
+		globalrole.SetupGated,
+		group.SetupGated,
+		httpssosettings.SetupGated,
+		oidcconfiguration.SetupGated,
+		reverseproxy.SetupGated,
+		samlsettings.SetupGated,
+		providerconfig.SetupGated,
+	} {
+		if err := setup(mgr, o); err != nil {
+			return err
+		}
+	}
+	return nil
+}
