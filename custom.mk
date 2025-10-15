a3362a7c57b15a5e43e8b0b8b5da8310be873637
Ondrej Homolka <ondrej.homolka@merck.com>
2025-10-15 15:11:14 +0200

    Add up CLI install

--- custom.mk
+++ custom.mk
@@ -1,8 +1,26 @@
+### UP CLI
+# up CLI was removed from build module, but crossplane CLI doesn't support 'xpkg append' subcommand
+#   https://github.com/crossplane/build/commit/32266ec2df9b775cc23675131147009375fe9bb6
+# So, it's added here as a custom workaround
+UP_VERSION ?= v0.41.0
+UP_CHANNEL ?= stable
+UP := $(TOOLS_HOST_DIR)/up-$(UP_VERSION)
 
+# up download and install
+$(UP):
+	@$(INFO) installing up $(UP_VERSION)
+	@if [ ! -f $(UP) ]; then \
+		curl -fsSLo $(UP) --create-dirs https://cli.upbound.io/$(UP_CHANNEL)/$(UP_VERSION)/bin/$(SAFEHOST_PLATFORM)/up?source=build || $(FAIL) && \
+		chmod +x $(UP); \
+	fi
+	@$(OK) installing up $(UP_VERSION)
+
+### Set package extensions
 # 1: registry/org 2: repo
 define xpkg.set-extensions
 xpkg.set-extensions.$(1).$(2): $(UP)
 	@$(INFO) Setting extensions for package $(1)/$(2):$(VERSION)
+	@$(UP) version
 	@$(UP) xpkg append \
 		--extensions-root=./extensions \
 		$(1)/$(2):$(VERSION)
