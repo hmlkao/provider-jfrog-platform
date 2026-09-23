9a047a364513d6be170241b8f54419cf62d73ef7
Ondrej Homolka <ondrej.homolka@gmail.com>
2026-09-23 15:06:44 +0200

    Add GHA job to update Go language

--- scripts/bump_golangci_lint_version.sh
+++ scripts/bump_golangci_lint_version.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 
-# Bump version of Terraform provider
+# Replace version of golangci-lint
+
+# Usage:
+#   bump_golangci_lint_version.sh <new_version>
 
 set -euo pipefail
 IFS=$'\n\t'
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-current_version=${1}
-if [[ -z "${current_version}" ]]; then
-  echo "::error Current version is empty"
-  exit 1
-fi
-new_version=${2}
+new_version=${1}
 if [[ -z "${new_version}" ]]; then
   echo "::error New version is empty"
   exit 1
 fi
 
-main() {
+main () {
   # Use of sed -i'.tmp' is to make it compatible with both GNU and BSD sed. BSD sed requires an argument for the -i option, so we provide a temporary file extension '.tmp' to create a backup of the original file before making in-place edits. After the edits are made, we remove the temporary backup file.
-  sed -i'.tmp' "s|CROSSPLANE_VERSION = ${current_version}|CROSSPLANE_VERSION = ${new_version}|" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
-  sed -i'.tmp' "s|CROSSPLANE_CLI_VERSION = v${current_version}|CROSSPLANE_CLI_VERSION = v${new_version}|" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|GOLANGCILINT_VERSION ?= .*|GOLANGCILINT_VERSION ?= ${new_version}|" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|GOLANGCI_VERSION: '.*'|GOLANGCI_VERSION: 'v${new_version}'|" "${dir}/../.github/workflows/ci.yml" && rm "${dir}/../.github/workflows/ci.yml.tmp"
 }
 
 main "$@"
--- scripts/bump_tfm_provider_version.sh
+++ scripts/bump_tfm_provider_version.sh
@@ -7,22 +7,23 @@ IFS=$'\n\t'
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 current_version=${1}
-if [[ -z "$current_version" ]]; then
+if [[ -z "${current_version}" ]]; then
   echo "::error Current version is empty"
   exit 1
 fi
 new_version=${2}
-if [[ -z "$new_version" ]]; then
+if [[ -z "${new_version}" ]]; then
   echo "::error New version is empty"
   exit 1
 fi
 
 main() {
-  sed -i "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|" "$dir/../Makefile"
-  sed -i "s|terraform-provider-platform_v${current_version}|terraform-provider-platform_v${new_version}|" "$dir/../Makefile"
-  sed -i "s|jfrog/platform v${current_version}|jfrog/platform v${new_version}|g" "$dir/../README.md"
-  sed -i "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "$dir/../README.md"
-  sed -i "s|jfrog/platform/${current_version}|jfrog/platform/${new_version}|" "$dir/../README.md"
+  # Use of sed -i'.tmp' is to make it compatible with both GNU and BSD sed. BSD sed requires an argument for the -i option, so we provide a temporary file extension '.tmp' to create a backup of the original file before making in-place edits. After the edits are made, we remove the temporary backup file.
+  sed -i'.tmp' "s|export TERRAFORM_PROVIDER_VERSION ?= ${current_version}|export TERRAFORM_PROVIDER_VERSION ?= ${new_version}|g" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|terraform-provider-platform_v${current_version}|terraform-provider-platform_v${new_version}|g" "${dir}/../Makefile" && rm "${dir}/../Makefile.tmp"
+  sed -i'.tmp' "s|jfrog/platform v${current_version}|jfrog/platform v${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
+  sed -i'.tmp' "s|Terraform provider v${current_version}|Terraform provider v${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
+  sed -i'.tmp' "s|jfrog/platform/${current_version}|jfrog/platform/${new_version}|g" "${dir}/../README.md" && rm "${dir}/../README.md.tmp"
 }
 
 main "$@"
